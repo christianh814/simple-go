f4b57531d819425f3a14b3cd0dca57e0d44585d8
Christian Hernandez <christian@codefresh.io>
2022-06-25 14:39:37 -0700

    updated the way I read the file
    Signed-off-by: Christian Hernandez <christian@codefresh.io>

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"html/template"
+	"io"
 	"net/http"
 	"os"
 
@@ -9,7 +11,7 @@ import (
 )
 
 type AppConfig struct {
-	Config string
+	Config []string
 	Status string
 	Msg    string
 	Alert  string
@@ -41,20 +43,20 @@ func appRoot(w http.ResponseWriter, r *http.Request) {
 	// If file exists, get the contents, if not set defaults
 	if _, err := os.Stat(AppConfigFile); os.IsNotExist(err) {
 		config = AppConfig{
-			Config: "",
+			Config: []string{},
 			Status: "Unknown",
 			Alert:  "alert-warning",
 			Msg:    "This means the config file was not found and will use the default config",
 		}
 	} else {
 		// read the file, display any errors
-		content, err := os.ReadFile(AppConfigFile)
+		content, err := readFile(AppConfigFile)
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		config = AppConfig{
-			Config: string(content),
+			Config: content,
 			Status: "Success",
 			Alert:  "alert-success",
 			Msg:    "This means that the app was able to successfully read the config file.",
@@ -65,4 +67,38 @@ func appRoot(w http.ResponseWriter, r *http.Request) {
 	// Display index page from template
 	tmpl.Execute(w, config)
 
+}
+
+// readFile returns the contents of a file as a slice of string
+func readFile(file string) ([]string, error) {
+	// set up the return slice of string
+	var s = []string{}
+
+	// open the file, report any errors
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	// close the file when the function exists
+	defer f.Close()
+
+	// read the file line by line and convert it into a slice of string
+	reader := bufio.NewReader(f)
+	for {
+		line, _, err := reader.ReadLine()
+		//if we get to the end of the file, break out of this loop
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return nil, err
+		}
+
+		// append contents into a slice of string
+		s = append(s, string(line))
+
+	}
+
+	return s, nil
 }
