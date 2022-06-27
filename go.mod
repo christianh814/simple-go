49fe361cf74b48bbccd72c99e36bd63921ecff26
Christian Hernandez <christian@codefresh.io>
2022-06-26 21:00:39 -0700

    playing with logging middleware
    Signed-off-by: Christian Hernandez <christian@codefresh.io>

--- go.mod
+++ go.mod
@@ -7,4 +7,9 @@ require (
 	github.com/sirupsen/logrus v1.8.1
 )
 
-require golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+require github.com/felixge/httpsnoop v1.0.1 // indirect
+
+require (
+	github.com/gorilla/handlers v1.5.1
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+)
--- pkg/app/app.go
+++ pkg/app/app.go
@@ -18,6 +18,13 @@ func Start() {
 	router.HandleFunc("/", appRoot)
 	router.HandleFunc("/info", appInfo)
 
+	/* Another way of serving with logging middleware. Saving this for future reference
+
+	router.Handle("/", handlers.CombinedLoggingHandler(os.Stdout, http.HandlerFunc(appRoot)))
+	router.Handle("/info", handlers.CombinedLoggingHandler(os.Stdout, http.HandlerFunc(appInfo)))
+
+	*/
+
 	// try to start the app and log output
 	log.Info("Starting server on port " + HttpPort)
 	log.Fatal(http.ListenAndServe(":"+HttpPort, router))
